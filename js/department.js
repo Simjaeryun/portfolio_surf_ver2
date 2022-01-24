be9187b51f063a303a926530a00d1ee9c8a38e88
Simjaeryun <simjaeding@gmail.com>
2022-01-24 19:32:19 +0900

    member page 레이아웃 완료

--- js/department.js
+++ js/department.js
@@ -14,14 +14,20 @@ fetch('data/member.json')
             tags += `
                         <article class="member_profile">
                             <div class="member_img">
-                            <img src="${member.pic}">
+                                <img src="${member.pic}">
                             </div>
-                            <h2 class="member_name">
-                                ${member.name}
-                            </h2>
-                            <p class="member_info">
-                                ${member.position}
-                            </p>
+                            <div class="member_text">
+                                <div class="member_name">
+                                    ${member.name}
+                                </div>
+                                <div class="member_position">
+                                    ${member.position}
+                                </div>
+                                <p class="member_info">
+                                    ${member.info}
+                                </p>
+                            </div>
+            
                         </article>
                         `
         })
