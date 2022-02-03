08fb93071405f78471dd099ee70f06479cd8dcb1
Simjaeryun <simjaeding@gmail.com>
2022-02-03 17:30:15 +0900

    mobile 전용 header 제작 및 mobile반응형 완료

--- js/header.js
+++ js/header.js
@@ -1,11 +1,28 @@
 const gnb = document.querySelector("#gnb");
 const gnbBtns = gnb.querySelectorAll("li a");
 
+const btnCall = document.querySelector(".btnCall");
+const menuMo = document.querySelector(".header_m_nav");
 
+const closeBtns = document.querySelectorAll("section");
+
+for (const el of closeBtns) {
+    el.addEventListener("click", (e) => {
+        btnCall.classList.remove("on");
+        menuMo.classList.remove("on");
+    })
+}
 
 for (const el of gnbBtns) {
     el.addEventListener("click", e => {
         el.classList.add("on")
     })
 
-} 
+}
+
+btnCall.onclick = (e) => {
+    e.preventDefault();
+    btnCall.classList.add("on");
+    menuMo.classList.add("on");
+
+}
